25ea1d19609f93ce82e0015fae3aa182131bce3c
Chris Perez <chris.k.perez@gmail.com>
2016-03-16 11:22:10 -0700

    questions with console logs

--- app.js
+++ app.js
@@ -4,64 +4,86 @@ console.log('Aked user for name. Got ' + userName);
 
 var answer1 = prompt('Do you think I have a car?');
 answer1 = answer1.toLowerCase();
+console.log('used str.toLowerCase() to avoid case sensitivity issues in responses.');
 
 if (answer1 === 'yes' || answer1 === 'y') {
   alert('Wrong, ' + userName + '. I used to have one, but I sold it last month.');
+  console.log('user answered yes. Wrong.');
 } else if (answer1 === 'no' || answer1 === 'n') {
   alert('Correct, ' + userName + '. I have a motorcycle, though.');
+  console.log('user answered no. Correct.');
 } else {
   alert('That is not a valid response. Please only enter "yes/no" or "y/n."');
+  console.log('invalid response given');
 };
 
 var answer2 = prompt('Do you think my motorcycle is a sweet, loud, black-and-chrome, American-made machine?');
 answer2 = answer2.toLowerCase();
+console.log('used str.toLowerCase() to avoid case sensitivity issues in responses.');
 
 if (answer2 === 'yes' || answer2 === 'y') {
   alert('Wrong, ' + userName + '. It is white and Italian and beautiful. I do like a good Harley, though.');
+  console.log('user answered yes. Wrong.');
 } else if (answer2 === 'no' || answer2 === 'n') {
   alert('Correct, ' + userName + '. It is Italian, in fact. Moto Guzzi V7. Look it up.');
+  console.log('user answered no. Correct.');
 } else {
   alert('I am pretty sure I already told you... Please only enter "yes/no" or "y/n."');
+  console.log('invalid response given');
 };
 
 var answer3 = prompt('Had I been to Washington before coming here for Code Fellows?');
 answer3 = answer3.toLowerCase();
+console.log('used str.toLowerCase() to avoid case sensitivity issues in responses.');
 
 if (answer3 === 'yes' || answer3 === 'y') {
   alert('Correct, ' + userName + '. This is probably my 20th time here.');
+  console.log('used answered yes. Correct.');
 } else if (answer3 === 'no' || answer3 === 'n') {
   alert('Well, ' + userName + ', I had! Many, many times.');
+  console.log('user answered no. Wrong.');
 } else {
   alert('Come on. Please only enter "yes/no" or "y/n."');
+  console.log('invalid response given');
 };
 
 var answer4 = prompt('Am I over 30 years old?');
 answer4 = answer4.toLowerCase();
+console.log('used str.toLowerCase() to avoid case sensitivity issues in responses.');
 
 if (answer4 === 'yes' || answer4 === 'y') {
   alert('Wrong, ' + userName + '. The beard adds a few years.');
+  console.log('user answered yes. Wrong.');
 } else if (answer4 === 'no' || answer4 === 'n') {
   alert('Correct, ' + userName + '.');
+  console.log('user answered no. Correct.');
 } else {
   alert('Seriously. Please only enter "yes/no" or "y/n."');
+  console.log('invalid response given');
 };
 
 var answer5 = prompt('Do you know kung fu?');
 answer5 = answer5.toLowerCase();
+console.log('used str.toLowerCase() to avoid case sensitivity issues in responses.');
 
 if (answer5 === 'yes' || answer5 === 'y') {
   var bonus = prompt('Sweet, ' + userName + '. Can you teach me?');
   bonus = bonus.toLowerCase();
+  console.log('used str.toLowerCase() to avoid case sensitivity issues in responses.');
   if (bonus === 'yes' || bonus === 'y') {
     alert(userName + ', when do we start?');
+    console.log('user said yes! Going to learn kung fu');
   } else if (bonus === 'no' || bonus === 'n') {
     alert('Fine, ' + userName + '. Be that way.');
+    console.log('user said no. user is lame.');
   } else {
     alert('How did you get this far without learning how to answer the questions properly? Go away.');
+    console.log('invalid response given');
   };
-
 } else if (answer5 === 'no' || answer5 === 'n') {
-  alert('Then you are of no use to me, ' + userName + '.');
+  alert('Then you are of no use to me, ' + userName + '. Bye.');
+  console.log('user answered no. Game over.');
 } else {
   alert('You know that was invalid. You are done.');
+  console.log('invalid response given');
 };
